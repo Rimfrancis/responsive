d532390f3ddc08d549c9d2f9220e14b845c3002f
Rimfrancis <148169130+Rimfrancis@users.noreply.github.com>
2023-11-10 17:25:21 +0800

    Add files via upload

---
+++ Land/func.js
@@ -0,0 +1,34 @@
+function togglem1() {
+    const menu = document.querySelector(".m1-links");
+    const icon = document.querySelector(".h1-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  
+const hamburger = document.querySelector(".nav__hamburger");
+const linksContainer = document.querySelector(".nav__menu");
+const links = document.querySelectorAll(".nav__menu__link");
+
+hamburger.addEventListener("click", () => {
+  linksContainer.classList.toggle("active");
+  hamburger.classList.toggle("active");
+});
+
+window.addEventListener("resize", () => {
+  if (window.matchMedia("(max-width: 550px)").matches) {
+    closeMenu();
+  }
+});
+
+if (window.matchMedia("(max-witdh: 550px").matches) {
+  closeMenu();
+}
+
+function closeMenu() {
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      linksContainer.classList.remove("active");
+      hamburger.classList.remove("active");
+    });
+  });
+}
